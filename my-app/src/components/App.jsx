c70d9fc0ebfee47fe4a7d89c0a95d6a0f8df55bd
sujithkumara <sujith.sunu19@gmail.com>
2023-02-19 06:42:42 +0000

    Keeper aap challenge 1

---
+++ my-app/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Notes";
+
+function App(){
+  return (<div> 
+    <Header />
+    <Note />
+    <Footer />
+  </div>);  
+}
+
+export default App;
---
+++ my-app/src/components/Notes.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Note(){
+  return <div className="note">
+    <h1> This is a Title</h1>
+    <p> This is a Content</p>
+  </div>
+}
+export default Note;
